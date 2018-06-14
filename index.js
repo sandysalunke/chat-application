8b35815562989e2340529bfc6fc8adfa5ce30abf
sandysalunke <sandip.salunke@outlook.com>
2018-06-14 16:12:56 +0530

    Changed listning port and added main file in package.json

--- index.js
+++ index.js
@@ -52,7 +52,10 @@ io.on('connection', function(socket){
 
 });
 
-// Listen application request on port 3000
-http.listen(3000, function(){
-  console.log('listening on *:8080');
+// Listen application request on port OPENSHIFT_NODEJS_PORT OR 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
